d0c41e3da1367646cb59d3ac32c76840dfab6785
Nikita Slobozhaninov <nikita-slb@yandex.ru>
2019-05-01 15:19:34 +0300

    Update Tabs.test.js
    add tests

--- src/lib/__tests__/Tabs.test.js
+++ src/lib/__tests__/Tabs.test.js
@@ -1,9 +1,9 @@
 /* eslint getter-return: "off" */
 
 import React from 'react';
-import { Tabs } from '../index';
+import { Tabs, Tab } from '../index';
 import renderer from 'react-test-renderer';
-import { configure } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -11,11 +11,55 @@ configure({ adapter: new Adapter() });
 describe('Tabs', () => {
   it('renders properly', () => {
     const tree = renderer
-        .create(
-                <Tabs/>
-            )
-            .toJSON();
+    .create(
+      <Tabs/>
+    )
+    .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it('test select tab handler', () => {
+    const clickFn = jest.fn(activeTabIndex => activeTabIndex);
+    const selectedTabIndex = 1;
+
+    const TabsComponent = mount((
+      <Tabs activeTabIndex={0} selectTab={clickFn}>
+        <Tab tabName="Tab 1">Tab 1</Tab>
+        <Tab tabName="Tab 2">Tab 2</Tab>
+      </Tabs>
+    ));
+
+    TabsComponent
+      .find('li.Tab')
+      .at(selectedTabIndex)
+      .simulate('click');
+
+    expect(clickFn).toHaveReturnedWith(selectedTabIndex);
+
+    TabsComponent.unmount();
+  });
+
+  it('test set active tab handler', () => {
+    const clickFn = jest.fn();
+    const defaultTabIndex = 0;
+    const selectedTabIndex = 1;
+
+    const TabsComponent = mount((
+      <Tabs activeTabIndex={defaultTabIndex} selectTab={clickFn}>
+        <Tab tabName="Tab 1">Tab 1</Tab>
+        <Tab tabName="Tab 2">Tab 2</Tab>
+      </Tabs>
+    ));
+
+    expect(TabsComponent.prop('activeTabIndex')).toEqual(defaultTabIndex);
+    expect(TabsComponent.state('activeTabIndex')).toEqual(defaultTabIndex);
+
+    TabsComponent.setProps({ activeTabIndex: selectedTabIndex });
+
+    expect(TabsComponent.prop('activeTabIndex')).toEqual(selectedTabIndex);
+    expect(TabsComponent.state('activeTabIndex')).toEqual(selectedTabIndex);
+
+    TabsComponent.unmount();
   });
 });
